09448970331472a30befdf455732fd48dff3db56
Swasti Sargam <117828125+Swas04@users.noreply.github.com>
2023-09-18 10:47:31 +0000

    day1-basic  cpp

---
+++ basiscpp/firstcode.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+// Main function where the program starts execution
+int main() {
+    // Your code goes here
+    
+    // Print a message to the console
+    std::cout << "Hello, World!" << std::endl;
+    
+    // Return an exit code to the operating system (0 usually means success)
+    return 0;
+}
